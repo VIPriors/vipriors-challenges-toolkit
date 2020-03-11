890eaffe7cf14828eb77a061589b3e02158dbd9f
Osman Semih Kayhan <o.s.kayhan@tudelft.nl>
2020-03-11 17:59:14 +0100

    Update vgg_fconv.py

--- image-classification/vgg_fconv.py
+++ image-classification/vgg_fconv.py
@@ -79,7 +79,7 @@ def make_layers(cfg, batch_norm=False):
         if v == 'M':
             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
         else:
-            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=2)#, bias=None)
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=2)
             if batch_norm:
                 layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
             else:
