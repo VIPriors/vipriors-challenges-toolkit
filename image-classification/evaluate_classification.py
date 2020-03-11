d2ab498c50149aed72d19cf57c977bb4429c4752
Robert-Jan Bruintjes <r.j.bruintjes@gmail.com>
2020-03-11 17:14:58 +0100

    Optimize classification evaluation.

--- image-classification/evaluate_classification.py
+++ image-classification/evaluate_classification.py
@@ -21,32 +21,38 @@ def evaluate_from_files(groundtruths_filepath, predictions_filepath, output_dir)
     output_dir = output_dir
     data = pd.read_csv(groundtruths_filepath)
     sub_data = pd.read_csv(predictions_filepath)
-    
+
     ground_truth = data.to_numpy()
     submission = sub_data.to_numpy()
-    
-    
+
+    indexed_gt = {}
+    for idx in range(len(ground_truth)):
+        indexed_gt[ground_truth[idx][0]] = ground_truth[idx]
+
+    indexed_sbm = {}
+    for idx in range(len(submission)):
+        indexed_sbm[submission[idx][0]] = submission[idx]
+
     tp = 0.0
     fp = 0.0
-    for idx in range(len(ground_truth)):
-        for idx2 in range(len(submission)):
-            if ground_truth[idx][0] == submission[idx2][0]:
-                if ground_truth[idx][1] == submission[idx2][1]:
-                    tp += 1
-                else:
-                    fp += 1
-                break
-    acc =  tp / (tp+fp)
-    
+    for im_idx in indexed_gt:
+        if im_idx not in indexed_sbm:
+            continue
+        if indexed_gt[im_idx][1] == indexed_sbm[im_idx][1]:
+            tp += 1.
+        else:
+            fp += 1.
+    acc = tp / (tp+fp)
+
     print('accuracy', acc)
-    
+
     metrics = [("Top1 accuracy", acc)]
     with open(os.path.join(output_dir, OUTPUT_FILE), 'w') as f:
             for name, val in metrics:
                 f.write(f"{name}: {val:.8f}\n")
-    
+
     print("Metrics written to scores.txt.")
-    
+
 if __name__ == '__main__':
     args = sys.argv[1:]
     evaluate_from_files(args[0], args[1], args[2])
